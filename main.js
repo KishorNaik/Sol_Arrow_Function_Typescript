b7fac9ef11cbbc4fd7be2c5d6ea02465499ab0e5
Kishor Naik <kishor.naik011.net@gmail.com>
2019-07-29 18:27:22 +0530

    code commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var funcArrow = function (leNumValue1, leNumValue2) {
+    return leNumValue1 + leNumValue2;
+};
+console.log(funcArrow(2, 2));
---
+++ main.ts
@@ -0,0 +1,5 @@
+let funcArrow = (leNumValue1: number, leNumValue2: number): number => {
+  return leNumValue1 + leNumValue2;
+};
+
+console.log(funcArrow(2, 2));
